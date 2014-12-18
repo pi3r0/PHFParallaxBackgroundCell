a0930f1679c7a402fffc25b17867a8b73382fe5b
Pierre Houguet <pierre@procheo.fr>
2014-12-18 21:26:50 +0100

    PHFParallaxBackgroundCell
    version 1.1
    
    - add Custom parameters
    - ASYNC image request.

---
+++ PHFParallaxBackgroundCellExample/PHFParallaxBackgroundCellExample/PHFGetAsyncBackgroundImage.swift
@@ -0,0 +1,13 @@
+//
+//  PHFGetAsyncBackgroundImage.swift
+//  PHFParallaxBackgroundCellExample
+//
+//  Created by Pierre Houguet on 18/12/2014.
+//  Copyright (c) 2014 Pierre Houguet. All rights reserved.
+//
+
+import UIKit
+
+class PHFGetAsyncBackgroundImage: NSObject {
+   
+}
